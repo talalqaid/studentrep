02e88131109bba36ec98bec648ccc04a6d739022
talalqaid <66565062+talalqaid@users.noreply.github.com>
2024-02-13 15:42:52 +0000

    first commit

---
+++ src/main/java/com/example/studentpro/StudentproApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/studentpro/StudentproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
